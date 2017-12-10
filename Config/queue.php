fa4438703a7be928e5896ec355cf70cc378f00c2
zhangfengjin <793665131@qq.com>
2017-12-10 23:58:54 +0800

    队列配置文件

---
+++ Config/queue.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fengjin1
+ * Date: 2017/12/5
+ * Time: 21:52
+ */
+return [
+    'default' => 'redis',
+    'connections' => [
+        'redis' => [
+            'driver' => 'redis',
+            'connection' => 'default'
+        ],
+
+    ],
+
+];
