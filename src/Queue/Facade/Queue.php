8a519f93304e67845242bfe24216ebe8228d9d24
zhangfengjin <793665131@qq.com>
2017-12-10 23:55:46 +0800

    修改art启动队列方式

---
+++ src/Queue/Facade/Queue.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fengjin1
+ * Date: 2017/12/10
+ * Time: 23:13
+ */
+
+namespace XYLibrary\Queue\Facade;
+
+
+class Queue
+{
+
+}
---
+++ src/Queue/QueueManagerServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fengjin1
+ * Date: 2017/12/10
+ * Time: 23:24
+ */
+
+namespace XYLibrary\Queue;
+
+
+class QueueManagerServiceProvider
+{
+
+}
